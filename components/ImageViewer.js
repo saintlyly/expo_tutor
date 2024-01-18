9fa63dbcddccbc4b672f9ce6146875a32d2c98b1
luckt <lucktao8@163.com>
2024-01-18 10:30:05 +0800

    build a screen

---
+++ components/ImageViewer.js
@@ -0,0 +1,15 @@
+import { StyleSheet,Image,View } from "react-native";
+
+export default function ImageViewer({placeholderImageSource}){
+    return (
+        <Image source={placeholderImageSource} style={styles.image} />
+      );
+}
+
+const styles=StyleSheet.create({
+    image: {
+        width: 320,
+        height: 440,
+        borderRadius: 18,
+      },
+})
